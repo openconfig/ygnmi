cf25530a3d2a0fe73d207e4f905e3001999cec05
Daniel Grau <dgrau@google.com>
2022-04-15 10:18:48 -0700

    Add unmarshal from ondatra (#3)
    * Add unmarshal from ondatra
    * feedback

---
+++ ygnmi/ygnmi.go
@@ -0,0 +1,77 @@
+// Copyright 2022 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package ygnmi contains gNMI client library for use with a ygot Schema.
+package ygnmi
+
+import (
+	"time"
+
+	"github.com/openconfig/ygot/ygot"
+	"github.com/openconfig/ygot/ytypes"
+
+	gpb "github.com/openconfig/gnmi/proto/gnmi"
+)
+
+// AnyQuery is a generic gNMI query for wildcard or non-wildcard state or config paths.
+// Supported operations: Batch.
+type AnyQuery[T any] interface {
+	// pathStruct returns to path struct for this query.
+	pathStruct() ygot.PathStruct
+	// fieldname returns the name of YANG directory schema entry.
+	// For leaves, this is the parent entry.
+	fieldName() string
+	// goStruct returns the struct that query should be unmarshalled into.
+	// For leaves, this is the parent.
+	goStruct() ygot.ValidatedGoStruct
+	// extract is used for leaves to return the field from the parent GoStruct.
+	// For non-leaves, this casts the GoStruct to the concrete type.
+	extract(ygot.ValidatedGoStruct) T
+	// isState returns if the path for this query is a state node.
+	isState() bool
+	// isLeaf returns if the path for this query is a leaf.
+	isLeaf() bool
+	// schema returns the root schema used for unmarshalling.
+	schema() *ytypes.Schema
+}
+
+// Value contains a value received from a gNMI request and its metadata.
+type Value[T any] struct {
+	val     T
+	present bool
+	// Path is the sample's YANG path.
+	Path *gpb.Path
+	// Timestamp is the sample time.
+	Timestamp time.Time
+	// RecvTimestamp is the time the test received the sample.
+	RecvTimestamp time.Time
+	// ComplianceErrors contains the compliance errors encountered from an Unmarshal operation.
+	ComplianceErrors *ComplianceErrors
+}
+
+// SetVal sets the value and marks it present.
+func (v *Value[T]) SetVal(val T) {
+	v.val = val
+	v.present = true
+}
+
+// Val returns the val and whether it is present.
+func (v *Value[T]) Val() (T, bool) {
+	return v.val, v.present
+}
+
+// IsPresent returns whether the value is present.
+func (v *Value[T]) IsPresent() bool {
+	return v.present
+}
