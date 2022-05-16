8f8174ef58fb942ea2d837ca53de2cda1763a6c5
DanG100 <dgrau@google.com>
2022-05-16 19:34:38 +0000

    Package godoc

--- app/ygnmi/cmd/root.go
+++ app/ygnmi/cmd/root.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package cmd contains root command for the ygnmi executable.
 package cmd
 
 import (
