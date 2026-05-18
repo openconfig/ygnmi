16d946e267e3c63d2927acaad23e61cb476c6a21
dplore <dloher@google.com>
2026-05-18 13:47:13 -0700

    Add comment for cliASCIIConfig and move to top.

--- ygnmi/gnmi.go
+++ ygnmi/gnmi.go
@@ -38,6 +38,8 @@ import (
 )
 
 const (
+	// cliASCIIConfig contains the content of an origin CLI configuration.
+	type cliASCIIConfig string
 	// OriginOverride is the key to custom opt that sets the path origin.
 	OriginOverride = "origin-override"
 	// cliOrigin is the  path origin string for CLI-originated payloads.
@@ -506,8 +508,6 @@ const (
 	updatePath
 	unionreplacePath
 )
-
-type cliASCIIConfig string
 
 // populateSetRequest fills a SetResponse for a val and operation type.
 func populateSetRequest(req *gpb.SetRequest, path *gpb.Path, val any, op setOperation, preferShadowPath, isLeaf bool, compressInfo *CompressionInfo, opts ...Option) error {
